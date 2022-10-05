79d4caaeb6229d04122a9201f0f79c9271380272
Ain Chung <artnouveau0804@gmail.com>
2022-10-05 20:06:56 +0900

    chore: install tailwindcss

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -5,7 +5,7 @@ import Seo from '../components/base/Seo';
 const Home: NextPage = () => {
   return (
     <>
-      <Seo title='홈 | 소상공간' content='소상공인 위주의 맛집 추천 웹사이트' />
+      <Seo title="홈 | 소상공간" content="소상공인 위주의 맛집 추천 웹사이트" />
       <div>홈</div>
       <LoginModal />
     </>
